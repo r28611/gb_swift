5570605ef4ce945e8c49bace19f37f80155a705f
Margarita Novokh <r28611@gmail.com>
2020-11-24 12:24:27 +0100

    ver 2. class Ballon

--- Novokn.playground/Contents.swift
+++ Novokn.playground/Contents.swift
@@ -1,66 +1,43 @@
 import UIKit
 
-enum DoorsState {
-    case open, close
-}
-
-enum EngineState {
+enum FareState {
     case isOn, isOff
 }
 
-enum TrunkAction {
-    case loadTrunk(Int)
-    case unloadTrunk(Int)
-}
-
 enum Action {
-    case doors, engine
+    case changeFireState
+    case putSandbag(Int)
+    case abortSandbag
 }
 
-struct SportCar {
-    let mark: String
-    let release: Int
-    let maxVolume: Int
-    var volume: Int = 0
-    var engineState: EngineState = .isOff
-    var doorState: DoorsState = .close
+struct Ballon {
+    let color: UIColor
+    let label: String
+    var sandbag: Int = 0
+    var fireState: FareState = .isOff
+    var height: Int = 0
     
     mutating func makeAction (action: Action) {
         switch action {
-        case .doors:
-            doorState = doorState == .close ? .open : .close
-        case .engine:
-            engineState = engineState == .isOff ? .isOn : .isOff
+        case .changeFireState:
+            fireState = fireState == .isOff ? .isOn : .isOff
+            print("Состояние огня - \(fireState)")
+        case .putSandbag(let number):
+            sandbag += number
+            print("Теперь есть \(sandbag) мешков с песком. Полетели!")
+        case .abortSandbag:
+            if sandbag - 1 <= 0 {
+                print("Больше мешков нет - пора выкидывать людей")
+            } 
+            sandbag -= 1
+            print("Теперь есть \(sandbag) мешков с песком")
         }
     }
     
-    mutating func makeTruncAction(trankAction: TrunkAction) {
-        switch trankAction {
-        case .loadTrunk(let load):
-            if volume + load > maxVolume {
-                print("НЕ ЛЕЗЕТ! Осталось место только для \(maxVolume - volume) кг добра")
-                return
-            } else {
-                volume += load
-            }
-        case .unloadTrunk(let load):
-            if volume - load == 0 {
-                print("Багажник теперь пустой")
-                volume -= load
-                return
-            } else if volume - load < 0 {
-                print("В багажнике только \(volume) кг добра")
-                return
-            } else {
-                volume -= load
-            }
-        }
-        print("Багажник заполнен на \(volume) из \(maxVolume) кг")
-    }
 }
 
-var car1 = SportCar(mark: "Zastava", release: 1980, maxVolume: 200)
-var car2 = SportCar(mark: "Lada", release: 1999, maxVolume: 100)
-car1.makeTruncAction(trankAction: .loadTrunk(50))
-car1.makeTruncAction(trankAction: .loadTrunk(150))
-car1.makeTruncAction(trankAction: .unloadTrunk(200))
+var ballon1 = Ballon(color: .blue, label: "Balkania")
+ballon1.makeAction(action: .putSandbag(2))
+ballon1.makeAction(action: .changeFireState)
+ballon1.makeAction(action: .abortSandbag)
+ballon1.makeAction(action: .abortSandbag)
